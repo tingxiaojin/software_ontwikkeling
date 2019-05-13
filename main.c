64165cd70e0099cbee7d917676151b12992ae933
Ting Xiao Jin <tingxiaojin@hotmail.com>
2019-05-13 08:58:33 +0200

    lijn_v2

--- main.c
+++ main.c
@@ -22,7 +22,7 @@
 // eerste drie bits of x +, -, =
 // tweede drie bits of y +, -, =
 
-int get_rc(int rc_dif, int noemer, int teller)
+int get_rc(int rc_dif, int teller, int noemer)
 {
 	int rc = (((float)teller+1)/((float)noemer+1))*100;	//berekening van helling
 
@@ -30,6 +30,8 @@ int get_rc(int rc_dif, int noemer, int teller)
 		return rc/100;		//afronden naar beneden
 	if(rc>=rc_dif)
 		return (rc/100)+1;	//afronden naar boven
+
+	return 0;
 }
 
 int switch_state(int x_dif, int y_dif){
@@ -51,7 +53,7 @@ int switch_state(int x_dif, int y_dif){
 	return switch_value;
 }
 //											noemer		teller	noemer	teller
-void lijn_hoek(float teller, float noemer, int BP1, int EP1, int BP2, int EP2, int kleur)
+void lijn_hoek(float teller, float noemer, int BP1, int EP1, int BP2, int EP2, int kleur, int graden)
 {
 	int i = BP1;
 	int j = EP1;
@@ -61,26 +63,21 @@ void lijn_hoek(float teller, float noemer, int BP1, int EP1, int BP2, int EP2, i
 	int rc_dif = (teller/noemer)*100;
 	for(i=BP1; i<= BP2; i++)
 	{
-						//tellery noemerx
-		rc = get_rc(rc_dif, EP2-j, EP2-i);
+		if(graden == 2)
+			rc = 1;
+		else					//tellery noemerx
+			rc = get_rc(rc_dif, EP2-j, BP2-i);
+
 		for(k=0; k<rc; k++)
 		{
-			UB_VGA_SetPixel(i,j,kleur);
+			if(graden > 0)
+				UB_VGA_SetPixel(i,j,kleur);
+			if(graden == 0)
+				UB_VGA_SetPixel(j,i,kleur);
 			j++;
 		}
 
 	}
-
-	/*rc_dif = ((float)y_dif/(float)x_dif)*100;
-				for(x=x_1; x<=x_2; x++)
-				{
-					rc = get_rc(rc_dif, x_2-x, y_2-y);
-					for(i=0; i<rc; i++)
-					{
-						UB_VGA_SetPixel(x,y,color);
-						y++;
-					}
-				}*/
 
 }
 
@@ -91,115 +88,30 @@ int API_draw_line(int x_1, int y_1, int x_2, int y_2, int color, int weight, int
 	char state;
 	state = switch_state(x_dif, y_dif);
 
-	int rc;
-	int rc_dif;
-	int i;
-	int x = x_1;
-	int y = y_1;
-
 	switch(state){
 	case 9://++
 		x_dif = x_2-x_1;
 		y_dif = y_2-y_1;
-		if(x_dif <= y_dif)
+		if(x_dif < y_dif)
 		{
-			/*rc_dif = ((float)y_dif/(float)x_dif)*100;
-			for(x=x_1; x<=x_2; x++)
-			{
-				rc = get_rc(rc_dif, x_2-x, y_2-y);
-				for(i=0; i<rc; i++)
-				{
-					UB_VGA_SetPixel(x,y,color);
-					y++;
-				}
-			}*/
 			//						noemer		teller	noemer	teller
-			lijn_hoek(y_dif, x_dif, x_1, y_1, x_2, y_2,color);
+			lijn_hoek(y_dif, x_dif, x_1, y_1, x_2, y_2,color, 1);
 		}
 		if(x_dif > y_dif)
 		{
-			/*rc_dif = ((float)x_dif/(float)y_dif)*100;
-			for(y=y_1; y<=y_2; y++)
-			{
-				rc = get_rc(rc_dif, y_2-y, x_2-x);
-				for(i=0; i<rc; i++)
-				{
-					UB_VGA_SetPixel(x,y,color);
-					x++;
-				}
-			}*/
 //						noemer		teller	noemer	teller
-			lijn_hoek(x_dif, y_dif, y_1, x_1, y_2, x_2,color);
+			lijn_hoek(x_dif, y_dif, y_1, x_1, y_2, x_2,color, 0);
+		}
+		if(x_dif == y_dif)
+		{
+			lijn_hoek(y_dif, x_dif, x_1, y_1, x_2, y_2,color, 2);
 		}
 		break;
 	default:
 		UB_VGA_SetPixel(100,100,VGA_COL_MAGENTA);
 		break;
 	}
-
-}
-
-//
-//parameter: x,y x',y' dikte kleur
-//
-void lijn(int x, int y, int x1, int y1, int size, int kleur){
-
-	int x_dif;
-	int y_dif;
-	int buffer;
-
-	//Deze twee if-statements is voor het plaatsen van een lijn van links naar recht
-	//buffer zorgt ervoor dat x is gewisseld
-	//het verschil tussen x coordinaten is voor het bepalen van welke berekening uitgevoerd moet worden
-	if(x1 < x)
-	{
-		buffer = x;
-		x = x1;
-		x1 = buffer;
-	}
-	x_dif = x1 - x;
-
-	if(y1 < y)
-	{
-		buffer = y;
-		y = y1;
-		y1 = buffer;
-	}
-	y_dif = y1 - y;
-
-	int rc;
-	int i ;
-	int j;
-
-	//berekening voor het maken van 0 tot 45 graden
-	if(y_dif<x_dif)
-	{
-		for(i=y; i<=y1; i++)
-		{
-	 		rc = (x1-x)/(y1-i+1);
-
-	 		for(j=0; j<=rc; j++)
-			{
-				UB_VGA_SetPixel((int)x,i,kleur);
-				x++;
-			}
-
-		}
-	}
-	//berekening voor het maken van 45 tot 90 graden
-	if(y_dif>=x_dif)
-	{
-		for(i=x; i<=x1; i++)
-		{
-			rc = (y1-y)/(x1-i+1);
-			for(j=0; j<=rc; j++)
-			{
-				UB_VGA_SetPixel(i,y,kleur);
-				y++;
-			}
-
-		}
-	}
+	return 0;
 }
 
 void rechthoek(int x_lo, int y_lo, int x_rb, int y_rb, int kleur){
@@ -232,11 +144,12 @@ int main(void)
 
 
 
-	API_draw_line(0,0,0,239,VGA_COL_CYAN,1,0);
+
 	UB_VGA_SetPixel(0,0,VGA_COL_RED);
 	UB_VGA_SetPixel(319,239,VGA_COL_RED);
 	API_draw_line(0,0,100,239,VGA_COL_GREEN,1,0);
 	API_draw_line(0,0,319,239,VGA_COL_YELLOW,1,0);
+	API_draw_line(0,0,1,239,VGA_COL_CYAN,1,0);
 
 
   while(1)
