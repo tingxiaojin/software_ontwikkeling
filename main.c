5f1204aef1179a414f9b92b9e65b4c0bf04cef9c
Ting Xiao Jin <tingxiaojin@hotmail.com>
2019-05-14 22:16:16 +0200

    V3.0

--- main.c
+++ main.c
@@ -13,55 +13,94 @@
 #include "main.h"
 #include "stm32_ub_vga_screen.h"
 #include <math.h>
-
-
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int API_draw_line(int x_1, int y_1, int x_2, int y_2, int color, int weight, int reserved)
 {
-  int i;
-  int x, y;
-  float dx, dy, a;
+	int i;
+	int x, y;
+	float dx, dy, rc;
 
-  dx = x_2 - x_1;
-  dy = y_2 - y_1;
-  a  = dy/dx;
-for(i=weight/2; weight>0; i--, weight--)
+	dx = x_2 - x_1;
+	dy = y_2 - y_1;
+	rc  = dy/dx;
+	for(i=weight/2; weight>0; i--, weight--)
+	{
+
+	if (fabs(rc)<=1)
+		if (x_1 < x_2)
+		  for (x=x_1; x<x_2; x++)
+		  {
+			y = (int)(rc*(x-x_1)+y_1+i); // y = ax + b
+			UB_VGA_SetPixel(x,y,color);
+		  }
+		else
+		  for (x=x_2; x<x_1; x++)
+		  {
+			y = (int)(rc*(x-x_1)+y_1+i); // y = ax + b
+			UB_VGA_SetPixel(x,y,color);
+		  }
+
+	  else
+		if (y_1 < y_2)
+		  for (y=y_1; y<y_2; y++)
+		  {
+			x = (int)((1/rc)*(y-y_1)+x_1+i); // x = (y - b)/a
+			UB_VGA_SetPixel(x,y,color);
+		  } else
+		  for (y=y_2; y<y_1; y++)
+		  {
+			x = (int)((1/rc)*(y-y_1)+x_1+i); // y = ax + b
+			UB_VGA_SetPixel(x,y,color);
+		  }
+	}
+
+	return 0;
+}
+
+int API_draw_rectangle(int x, int y, int width, int height, int color, int filled, int reserved, int reserved1)
 {
+	int i;
+	int j;
+	int rand;
 
-	if (fabs(a)<=1)
-    if (x_1 < x_2)
-      for (x=x_1; x<x_2; x++)
-      {
-        y = (int)(a*(x-x_1)+y_1+i); // y = ax + b
-        UB_VGA_SetPixel(x,y,color);
-      }
-    else
-      for (x=x_2; x<x_1; x++)
-      {
-        y = (int)(a*(x-x_1)+y_1+i); // y = ax + b
-        UB_VGA_SetPixel(x,y,color);
-      }
+	for(j=y; j<=y+height; j++)
+	{
+		if(y<rand || y>y+h-rand)
+			continue;
+		else
+			if(filled == 1)
+				continue;
+			else
+				break;
+		for(i=x; i<=x+width; i++)
+		{
+			if(x<rand || x>x+w-rand)
+				UB_VGA_SetPixel(i,j,color);
+			else
+				if(filled == 1)
+					UB_VGA_SetPixel(i,j,color);
+				else
+					break;
+		}
+	}
 
-  else
-    if (y_1 < y_2)
-      for (y=y_1; y<y_2; y++)
-      {
-        x = (int)((1/a)*(y-y_1)+x_1+i); // x = (y - b)/a
-        UB_VGA_SetPixel(x,y,color);
-      } else
-      for (y=y_2; y<y_1; y++)
-      {
-        x = (int)((1/a)*(y-y_1)+x_1+i); // y = ax + b
-        UB_VGA_SetPixel(x,y,color);
-      }
+	return 0;
 }
 
-  return 0;
+int API_clearscreen (int color)
+{
+	UB_VGA_FillScreen(color);
+	return 0;
 }
+
 
 int main(void)
 {
-
+\
 	//  uint32_t n;
 
 	SystemInit(); // System speed to 168MHz
@@ -73,14 +112,15 @@ int main(void)
 
 
 
-
 //	UB_VGA_SetPixel(10,10,10);
 
 
-	API_draw_line(0, 0, 319, 239, VGA_COL_WHITE, 10, 0);
+	API_draw_line(140, 100, 319, 239, VGA_COL_WHITE, 10, 0);
 	API_draw_line(319, 0, 0, 239, VGA_COL_WHITE, 10, 0);
 	API_draw_line(0, 0, 319, 239, VGA_COL_RED, 1, 0);
 	API_draw_line(319, 0, 0, 239, VGA_COL_RED, 1, 0);
+
+	API_draw_rectangle(10,10,50,100,VGA_COL_CYAN,0,0,0);
 
 
   while(1)
