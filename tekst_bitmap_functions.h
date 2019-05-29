9ceadcb990414f002da3d44e713bd0d19f59127b
tingxiaojin <tingxiaojin@hotmail.com>
2019-05-29 10:50:28 +0200

    final countdown

---
+++ tekst_bitmap_functions.h
@@ -0,0 +1,53 @@
+//--------------------------------------------------------------
+// File     : main.h
+//--------------------------------------------------------------
+
+//--------------------------------------------------------------
+#ifndef __TEKST_BITMAP_FUNCTIONS_H
+#define __TEKST_BITMAP_FUNCTIONS_H
+
+
+//--------------------------------------------------------------
+// Includes
+//--------------------------------------------------------------
+
+//--------------------------------------------------------------
+// Defines
+//--------------------------------------------------------------
+
+
+
+
+//--------------------------------------------------------------
+// Extern declared functions
+//--------------------------------------------------------------
+extern int 	 API_io_putc_0 			(int c,    int x, 	   int y,	  int kleur, int achtergrond);
+extern int   API_io_puts_0			(char* zin, int x_lup, int y_lup, int kleur, int reserved);
+extern int 	 API_io_putc_1 			(int c,    int x, 	   int y,	  int kleur, int achtergrond);
+extern int   API_io_puts_1			(char* zin, int x_lup, int y_lup, int kleur, int reserved);
+extern int 	 API_io_putc_2 			(int c,    int x, 	   int y,	  int kleur, int achtergrond);
+extern int   API_io_puts_2			(char* zin, int x_lup, int y_lup, int kleur, int reserved);
+extern int 	 API_io_putc_3 			(int c,    int x, 	   int y,	  int kleur, int achtergrond);
+extern int   API_io_puts_3			(char* zin, int x_lup, int y_lup, int kleur, int reserved);
+extern int 	 API_io_putc_4 			(int c,    int x, 	   int y,	  int kleur, int achtergrond);
+extern int   API_io_puts_4			(char* zin, int x_lup, int y_lup, int kleur, int reserved);
+extern int 	 API_io_putc_5 			(int c,    int x, 	   int y,	  int kleur, int achtergrond);
+extern int   API_io_puts_5			(char* zin, int x_lup, int y_lup, int kleur, int reserved);
+extern int 	 API_io_putc_6 			(int c,    int x, 	   int y,	  int kleur, int achtergrond);
+extern int   API_io_puts_6			(char* zin, int x_lup, int y_lup, int kleur, int reserved);
+extern int 	 API_io_putc_7 			(int c,    int x, 	   int y,	  int kleur, int achtergrond);
+extern int   API_io_puts_7			(char* zin, int x_lup, int y_lup, int kleur, int reserved);
+extern int 	 API_io_putc_8 			(int c,    int x, 	   int y,	  int kleur, int achtergrond);
+extern int   API_io_puts_8			(char* zin, int x_lup, int y_lup, int kleur, int reserved);
+extern int 	 API_io_putc_9 			(int c,    int x, 	   int y,	  int kleur, int achtergrond);
+extern int   API_io_puts_9			(char* zin, int x_lup, int y_lup, int kleur, int reserved);
+extern int 	 API_io_putc_10 		(int c,    int x, 	   int y,	  int kleur, int achtergrond);
+extern int   API_io_puts_10			(char* zin, int x_lup, int y_lup, int kleur, int reserved);
+extern int 	 API_io_putc_11 		(int c,    int x, 	   int y,	  int kleur, int achtergrond);
+extern int   API_io_puts_11			(char* zin, int x_lup, int y_lup, int kleur, int reserved);
+//--------------------------------------------------------------
+// Extern declared variables
+//--------------------------------------------------------------
+
+//--------------------------------------------------------------
+#endif // __STM32F4_UB_MAIN_H
